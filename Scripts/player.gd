200f91d53d4e194d42b47c9c2f506ad39ba08828
stefanakis <stefandunov@gmail.com>
2017-12-30 16:34:45 +0200

    Променени имена на функции и променливи - defining_tile; defining_mode; set_defining_mode

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 var WALK_MAX_VELOCITY = 200.0
 var is_jumping = false
-var programming_mode = false
+var defining_mode = false
 
 func _integrate_forces(state):
 	
@@ -12,7 +12,7 @@ func _integrate_forces(state):
 	var move_left = Input.is_action_pressed("move_left")
 	var move_right = Input.is_action_pressed("move_right")
 	var jump = Input.is_action_pressed("jump")
-	var program_tile = Input.is_action_pressed("program_tile")
+	var show_options = Input.is_action_pressed("show_options")
 	
 	#Movement
 	if(jump and not get_colliding_bodies().empty()):
@@ -34,12 +34,12 @@ func _integrate_forces(state):
 	if (get_rot() != 0):
 		set_rot(0)
 	
-	if (get_colliding_bodies().size() == 1 and program_tile and not programming_mode):
-		programming_mode = true
+	if (get_colliding_bodies().size() == 1 and show_options and not defining_mode):
+		defining_mode = true
 		#Sends signal to program the block (tile)
-		var tiles = get_colliding_bodies()[0].get_parent().programmable_tiles
+		var tiles = get_colliding_bodies()[0].get_parent().defining_tiles
 		for i in range(tiles.size()):
-			get_parent().get_tree().call_group(0, "tiles", "set_programmable", tiles[i][0], tiles[i][1])
+			get_parent().get_tree().call_group(0, "tiles", "set_defining_mode", tiles[i][0], tiles[i][1])
 		
 func _ready():
 	set_max_contacts_reported(2)
--- Scripts/tile.gd
+++ Scripts/tile.gd
@@ -3,23 +3,23 @@ extends Sprite
 var row
 var col
 var is_active
-var programmable_tiles = []
-var programming_mode = false
+var defining_tiles = []
+var defining_mode = false
 
 func set_active():
-	set_texture(load("res://tile2.png"))
+	set_texture(load("res://Textures/active_tile.png"))
 	get_node("StaticBody2D/CollisionShape2D").set_trigger(false)
 	is_active = true
 	
 func set_inactive():
-	set_texture(load("res://tile1.png"))
+	set_texture(load("res://Textures/inactive_tile.png"))
 	get_node("StaticBody2D/CollisionShape2D").set_trigger(true)
 	is_active = false
 
-func set_programmable(row, col):
+func set_defining_mode(row, col):
 	if (self.row == row and self.col == col):
-		set_texture(load("res://tile3.png"))
-		programming_mode = true
+		set_texture(load("res://Textures/defining_tile.png"))
+		defining_mode = true
 
 func set_normal():
 	if (is_active):
@@ -32,7 +32,7 @@ func init(row, col):
 	self.col = col
 	for i in range(3):
 		randomize()
-		programmable_tiles.push_front([randi()%5+row+1, randi()%8])
+		defining_tiles.push_front([randi()%5+row+1, randi()%8])
 
 func _ready():
 	add_to_group("tiles")
@@ -40,10 +40,10 @@ func _ready():
 
 func _input(event):
 	var mouse_pos = get_global_mouse_pos() + Vector2(61, 45)
-	if(event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and programming_mode):
+	if(event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and defining_mode):
 		if(mouse_pos.x > get_pos().x and mouse_pos.y > get_pos().y and mouse_pos.x < get_pos().x + 128 and mouse_pos.y < get_pos().y + 96):
 			set_active()
-			programming_mode = false
-			get_parent().get_node("Player").programming_mode = false
+			defining_mode = false
+			get_parent().get_node("Player").defining_mode = false
 			get_parent().get_tree().call_group(0, "tiles", "set_normal")
 	
